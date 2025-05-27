2e5d6768516faad4ffa156060d154628d2457abd
Maksym <maksym@viggo-sntv.com>
2025-05-27 11:52:52 +0300

    SplashViewModel + MainActivity fixed

--- app/src/main/java/com/example/usercontactsapp/MainActivity.kt
+++ app/src/main/java/com/example/usercontactsapp/MainActivity.kt
@@ -3,15 +3,14 @@ package com.example.usercontactsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.getValue
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.rememberNavController
+import com.example.usercontactsapp.features.splash.SplashViewModel
 import com.example.usercontactsapp.ui.navigation.UserContactsNavGraph
 import com.example.usercontactsapp.ui.theme.UserContactsAppTheme
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.example.usercontactsapp.features.splash.SplashViewModel
+import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
@@ -19,26 +18,29 @@ class MainActivity : ComponentActivity() {
     private val splashViewModel: SplashViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen().setKeepOnScreenCondition {
-            splashViewModel.isLoading.value
-        }
-        splashViewModel.checkStartDestination()
+        var showSplash = true
+        installSplashScreen().setKeepOnScreenCondition { showSplash }
 
-        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
-        setContent {
-            UserContactsAppTheme {
-                val navController = rememberNavController()
-                val startDestination by splashViewModel.startDestination.collectAsStateWithLifecycle()
-                Surface {
-                    UserContactsNavGraph(
-                        navController = navController,
-                        startDestination = startDestination
-                    )
+        splashViewModel.checkStartDestination()
+
+        lifecycleScope.launch {
+            splashViewModel.startDestination.collect { destination ->
+                showSplash = false
+
+                setContent {
+                    val navController = rememberNavController()
+
+                    UserContactsAppTheme {
+                        Surface {
+                            UserContactsNavGraph(
+                                navController = navController, startDestination = destination
+                            )
+                        }
+                    }
                 }
             }
         }
     }
 }
-
--- app/src/main/java/com/example/usercontactsapp/features/splash/SplashViewModel.kt
+++ app/src/main/java/com/example/usercontactsapp/features/splash/SplashViewModel.kt
@@ -6,27 +6,22 @@ import com.example.usercontactsapp.data.repository.UserRepository
 import com.example.usercontactsapp.features.navigation.AppDestination
 import com.example.usercontactsapp.features.navigation.UserForm
 import com.example.usercontactsapp.features.navigation.UserInfo
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.Dispatchers
 
 class SplashViewModel(
     private val repository: UserRepository
 ) : ViewModel() {
 
-    private val _isLoading = MutableStateFlow(true)
-    val isLoading: StateFlow<Boolean> = _isLoading
-
-    private val _startDestination = MutableStateFlow<AppDestination>(UserForm)
-    val startDestination: StateFlow<AppDestination> = _startDestination
+    private val _startDestination = MutableSharedFlow<AppDestination>(replay = 1)
+    val startDestination: SharedFlow<AppDestination> = _startDestination.asSharedFlow()
 
     fun checkStartDestination() {
-        if (!_isLoading.value) return
-
         viewModelScope.launch(Dispatchers.IO) {
             repository.getUser().collect { user ->
-                _startDestination.value = if (user != null) UserInfo else UserForm
-                _isLoading.value = false
+                val destination = if (user != null) UserInfo else UserForm
+                _startDestination.emit(destination)
             }
         }
     }
